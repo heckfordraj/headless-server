1c6bb98096ffd94b20ac24845846fc4b68090e28
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2017-12-14 16:00:48 +0000

    added Page type key

--- app/collections.js
+++ app/collections.js
@@ -6,6 +6,10 @@ class Collections {
   constructor() {
 
     var pageSchema = mongoose.Schema({
+      type: {
+        type: String,
+        default: 'page'
+      },
       name: {
         type: String,
         required: true,
